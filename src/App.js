f6892e9b457a471a62f2bdceecd9b9c1b7fca94c
mahbub1626 <mahbub1626@gmail.com>
2022-09-23 12:04:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExternalUsers></ExternalUsers>
+      {/* <Counter></Counter> */}
     </div>
   );
 }
+
+/**
+ counter
+ */
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  // useEffect(()=> {},[])
+  // const myfunc =()=> {}
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return(
+    <div style={{color:'green', border: '3px solid red', margin: '20px' , borderRadius: '10px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+/**Counter */
+
+// function Counter() {
+//   const [count, setCount] = useState(55);
+
+//   const increaseCount = () => {setCount(count + 1)}
+//   const decreaseCount = () => {setCount(count - 1)}
+
+//   // const increaseCount = () => {
+//   //   const newCount = count + 1;
+//   //   setCount(newCount);
+//   // }
+//   return (
+//     <div>
+//       <h2>Counter: {count}</h2>
+//       <button onClick={increaseCount}>Increase</button>
+//       <button onClick={decreaseCount}>Decrease</button>
+//     </div>
+//   )
+// }
 
 export default App;
